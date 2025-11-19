b2486018bf21ed2f41f88b931baf68bdeaa8a509
Virendrawebdev <virendra052001@gmail.com>
2025-11-19 18:03:07 +0530

    Randomness| TextGenerate

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let btn=document.querySelector('.btn')
 let main=document.querySelector('main')
 
+let arr=['Hey!I am virndra...','Best of Luck!', 'Success is the sum of small efforts', 'Repeated day in and day out','Keep Practicing', 'JS is Love' ]
+ let  cl=['red','green', 'blue','purple','yellow']
 btn.addEventListener('click',function(){
- var div=document.createElement('div');
- let x = Math.floor(Math.random()*101)
- let y = Math.floor(Math.random()*101)
- let c1 = Math.floor(Math.random()*256)
- let c2 = Math.floor(Math.random()*256)
- let c3 = Math.floor(Math.random()*256)
- div.style.width='50px'
- div.style.height='50px'
- div.style.background=`rgb(${c1},${c2},${c3})`
- div.style.position="absolute"
- div.style.left=x +'%'
- div.style.top=y +'%'
- main.appendChild(div)
+ var h1=document.createElement('h1');
+ let a=Math.floor(Math.random()*arr.length)
+ let b=Math.floor(Math.random()*cl.length)
+ let x = Math.floor(Math.random()*80)
+ let y = Math.floor(Math.random()*80)
+ let r = Math.floor(Math.random()*360)
+ let fs = Math.floor(Math.random()*3)
+
+ h1.innerHTML=arr[a]
+ h1.style.position='absolute'
+ h1.style.left=x+'%'
+ h1.style.top=y+'%'
+ h1.style.scale=fs
+ h1.style.rotate=r+'deg'
+ h1.style.color=cl[b]
+ main.appendChild(h1)
 })

a45e77260ef1f148d3f4dbfbb30ad96ade9d58d2
Virendrawebdev <virendra052001@gmail.com>
2025-11-19 15:46:17 +0530

    Randomness| colorGenerate

--- script.js
+++ script.js
@@ -1,74 +1,18 @@
-var arr=[
-    {
-     team: 'CSK',
-     primary: 'yellow',
-     secondary: 'blue',
-     fullName: 'Chennai Super Kings'
-    },
-     {
-     team: 'SRH',
-     primary: 'Orange',
-     secondary: 'black',
-     fullName: 'Sunrisers Hyderabad'
-    },
-     {
-     team: 'GT',
-     primary: 'Deep blue',
-     secondary: 'Gold',
-     fullName: 'Gujarat Titans'
-    },
-     {
-     team: 'KKR',
-     primary: 'Purple',
-     secondary: 'gold',
-     fullName: 'Kolkata Knight Riders '
-    },
-     {
-     team: 'DC',
-     primary: 'blue',
-     secondary: 'red',
-     fullName: 'Delhi Capitals'
-    },
-     {
-     team: 'LSG',
-     primary: 'Royalblue',
-     secondary: 'blue',
-     fullName: 'Lucknow Super Giants'
-    },
-     {
-     team: 'RR',
-     primary: 'Pink',
-     secondary: 'Gold',
-     fullName: 'Rajasthan Royals'
-    },
-     {
-     team: 'PBKS',
-     primary: 'Red',
-     secondary: 'gold',
-     fullName: 'Punjab Kings'
-    },
-     {
-     team: 'RCB',
-     primary: 'Red',
-     secondary: 'Gold',
-     fullName: 'Royal Challenger Bangalore'
-    },
-     {
-     team: 'MI',
-     primary: 'Blue',
-     secondary: 'Gold',
-     fullName: 'Mumbai Indians'
-    },
-]
-
-
-var h2=document.querySelector('h2');
-var h3=document.querySelector('h3');
-var btn=document.querySelector('#btn');
+let btn=document.querySelector('.btn')
+let main=document.querySelector('main')
 
 btn.addEventListener('click',function(){
-    let ch =arr[Math.floor(Math.random()*arr.length)];
-    h2.innerHTML= `${ch.team}`
-    h3.innerHTML= `${ch.fullName}`
-    h2.style.backgroundColor= ch.primary
+ var div=document.createElement('div');
+ let x = Math.floor(Math.random()*101)
+ let y = Math.floor(Math.random()*101)
+ let c1 = Math.floor(Math.random()*256)
+ let c2 = Math.floor(Math.random()*256)
+ let c3 = Math.floor(Math.random()*256)
+ div.style.width='50px'
+ div.style.height='50px'
+ div.style.background=`rgb(${c1},${c2},${c3})`
+ div.style.position="absolute"
+ div.style.left=x +'%'
+ div.style.top=y +'%'
+ main.appendChild(div)
 })

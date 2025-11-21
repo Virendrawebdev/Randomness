02d076f0c05d20554443e3fb3ad4fc05a7635af6
Virendrawebdev <virendra052001@gmail.com>
2025-11-21 11:16:48 +0530

    Insta dbl like

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-var btn = document.querySelector('button')
-var h2 = document.querySelector('h2')
-var i = document.querySelector('.inner')
-var h3 = document.querySelector('h3')
-var a = 0;
-btn.addEventListener('click', () => {
-    btn.style.pointerEvents = 'none'
+var card=document.querySelector('.card')
+let like=document.querySelector('#like')
 
-    var num = 50 + Math.floor(Math.random() * 50)
-    console.log(num)
-    var stop = setInterval(function () {
-        a++
-        h2.innerHTML = a + '%'
-        i.style.width = a + '%'
-    }, num);
+card.addEventListener('dblclick',function(){
+    
+   like.style.opacity="1" 
+   like.style.transform="translate(-50%, -50%) scale(1) rotate(0deg)"
 
-    setTimeout(function () {
-        clearInterval(stop)
-        btn.innerHTML = "Downloaded"
-        btn.style.backgroundColor = 'red'
-        btn.style.opacity = '0.5'
-        h3.innerHTML = 'Downloaded in ' + `${num / 10}` + 'seconds';
+   setTimeout(function(){
+    like.style.transform="translate(-50%, -300%) scale(1) rotate(60deg)"
+   },800)
+   setTimeout(function(){
+    like.style.opacity="0" 
 
-    }, num * 100);
-
+   },1000)
+    setTimeout(function(){
+    like.style.transform="translate(-50%, -50%) scale(1) rotate(0deg)"
+   },1200)
 
 })

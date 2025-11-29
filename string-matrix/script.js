d6684d3d819fce8aa55b66b6a8e485fc6776dfd4
Virendrawebdev <virendra052001@gmail.com>
2025-11-29 12:01:04 +0530

    string-matrix

---
+++ string-matrix/script.js
@@ -0,0 +1,22 @@
+var para = document.querySelector('p')
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const text = para.innerText
+let iteration = 0
+function randomText() {
+    const str = text.split('').map((char, index) => {
+        if (index < iteration) {
+            return char
+        }
+        return characters.split('')[Math.floor(Math.random() * 52)]
+    }).join("")
+    para.innerText = str
+    iteration += 0.20
+}
+setInterval(randomText, 30)
+
+
+
+
+
+
+
